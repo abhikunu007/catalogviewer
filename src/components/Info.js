2f6f7e141b4b9140b4627f7285dff0b9ee0cf024
abhikunu007 <ak0879918@gmail.com>
2023-04-27 21:38:20 +0530

    completed

---
+++ src/components/Info.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Info() {
+  return (
+    <div>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium ut quia voluptas quam magni aliquam, ducimus iste autem eligendi sint tenetur illum, minus veniam repellendus aperiam voluptates qui magnam. Iure cum tempore nulla, facilis provident reiciendis modi quibusdam pariatur eius aliquam corrupti similique voluptates soluta asperiores rerum, ex, facere repellat non tempora? Voluptas aliquam pariatur corporis ipsa atque optio dignissimos eligendi sed expedita. </p>
+    </div>
+  )
+}
+
+export default Info
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Thumbs from './Thumbs'
+import Viewer from './Viewer'
+import Info from './Info'
+
+
+
+export { Thumbs, Viewer, Info }
